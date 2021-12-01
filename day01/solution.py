5acde66810df384cff7c8b0ac0c1508610417248
Jakob <jakobedvarderlandsson@gmail.com>
2021-12-01 07:56:05 +0100

    removed empty lines

--- day01/solution.py
+++ day01/solution.py
@@ -1,4 +1,3 @@
 from os import getenv
 input = [int(i) for i in open('input.txt', 'r').read()[:-1].split('\n')]
 print(sum([input[i+1] > input[i] for i in range(len(input)-1)]) if getenv('part') == 'part1' else sum([sum(input[i+1:i+4]) > sum(input[i:i+3]) for i in range(len(input)-3)]))
-
