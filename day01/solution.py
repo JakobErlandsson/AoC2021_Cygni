e102968c67824fdcbb5334df3fe666054528f468
Jakob <jakobedvarderlandsson@gmail.com>
2021-12-01 07:55:34 +0100

    day one

--- day01/solution.py
+++ day01/solution.py
@@ -1,16 +1,4 @@
 from os import getenv
-import numpy as np
-
 input = [int(i) for i in open('input.txt', 'r').read()[:-1].split('\n')]
+print(sum([input[i+1] > input[i] for i in range(len(input)-1)]) if getenv('part') == 'part1' else sum([sum(input[i+1:i+4]) > sum(input[i:i+3]) for i in range(len(input)-3)]))
 
-def get_primes(n):
-    sieve = [True] * (n//2)
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i//2]:
-            sieve[i*i//2::i] = [False] * ((n-i*i-1)//(2*i)+1)
-    return [2] + [2*i+1 for i in range(1,n//2) if sieve[i]]
-
-if getenv('part') == 'part1':
-    print(sum([i*n*(n in get_primes(max(input))) for (i,n) in enumerate(input)]))
-else: # part == 'part2'
-    print(sum([n*(n not in get_primes(max(input)))*(-1 if i%2 == 1 else 1) for (i,n) in enumerate(input)]))
