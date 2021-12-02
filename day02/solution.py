236acf307ff4c7e01339a451fca5cfc7e43d1897
Jakob <jakobedvarderlandsson@gmail.com>
2021-12-02 18:04:15 +0100

    removed a few lines

--- day02/solution.py
+++ day02/solution.py
@@ -1,10 +1,7 @@
 from os import getenv
 input = [i.split(' ') for i in open('input.txt', 'r').read()[:-1].split('\n')]
-if getenv('part') == 'part1': 
-    print(sum([int(i[1]) for i in input if i[0] == 'forward']) * sum([int(i[1]) if i[0] == 'down' else -int(i[1]) if i[0] == 'up' else 0 for i in input]))
-elif getenv('part') == 'part2':
-    (aim, depth) = (0,0)
-    for i in input:
-        aim += int(i[1]) if i[0] == 'down' else -int(i[1]) if i[0] == 'up' else 0 
-        depth += aim*int(i[1]) if i[0] == 'forward' else 0
-    print(depth*sum([int(i[1]) for i in input if i[0] == 'forward']))
+(aim, depth, dist) = (0,0, sum([int(i[1]) for i in input if i[0] == 'forward']))
+for i in input:
+    aim += int(i[1]) if i[0] == 'down' else -int(i[1]) if i[0] == 'up' else 0 
+    depth += aim*int(i[1]) if i[0] == 'forward' else 0
+print(dist * sum([int(i[1]) if i[0] == 'down' else -int(i[1]) if i[0] == 'up' else 0 for i in input]) if getenv('part') == 'part1' else dist*depth)
