0a4d89613a5def79a383a46aabf0b5208f558f7b
Jakob <jakobedvarderlandsson@gmail.com>
2021-12-09 16:50:18 +0100

    20% fewer lines, 50% less readable

--- day06/solution.py
+++ day06/solution.py
@@ -2,9 +2,7 @@ import sys, os
 input = [int(i) for i in open('input.txt', 'r').read()[:-1].split(',')]
 fish = [input.count(i) for i in range(10)]
 for t in range(256):
-    (new_8,new_6) = (fish[0],fish[0])
-    fish = [fish[i+1] for i in range(8)] + [new_8]
+    (new_8,new_6, fish) = (fish[0],fish[0], [fish[i+1] for i in range(8)] + [new_8])
     fish[6] += new_6
-    if os.getenv('part') == 'part1' and t == 79:
-        sys.exit("{}".format(sum([fish[i] for i in range(len(fish))])))
+    if os.getenv('part') == 'part1' and t == 79: sys.exit("{}".format(sum([fish[i] for i in range(len(fish))])))
 print(sum([fish[i] for i in range(len(fish))]))
