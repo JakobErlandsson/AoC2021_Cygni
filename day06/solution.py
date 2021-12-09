50a3a8d68a5a49c37f08231e166e2683f85fa2c8
Jakob <jakobedvarderlandsson@gmail.com>
2021-12-09 16:51:18 +0100

    Back to 8 lines and not even remotely readable

--- day06/solution.py
+++ day06/solution.py
@@ -2,7 +2,7 @@ import sys, os
 input = [int(i) for i in open('input.txt', 'r').read()[:-1].split(',')]
 fish = [input.count(i) for i in range(10)]
 for t in range(256):
-    (new_8,new_6, fish) = (fish[0],fish[0], [fish[i+1] for i in range(8)] + [new_8])
+    (fish, new_6) = ([fish[i+1] for i in range(8)] + [fish[0]],fish[0])
     fish[6] += new_6
     if os.getenv('part') == 'part1' and t == 79: sys.exit("{}".format(sum([fish[i] for i in range(len(fish))])))
 print(sum([fish[i] for i in range(len(fish))]))
